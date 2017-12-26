9f62736f623d894b172ecbf930bbc55af7b08f91
claudio <cmartinsgyn@gmail.com>
2017-12-25 23:48:06 -0200

    alteracao para postgres

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,10 +1,18 @@
-CREATE TABLE categoria (
-  codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- CREATE TABLE categoria (
+--   codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+--   nome VARCHAR(50) NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
 
-INSERT INTO categoria (nome) VALUES ('Lazer');
-INSERT INTO categoria (nome) VALUES ('Alimentação');
-INSERT INTO categoria (nome) VALUES ('Supermercado');
-INSERT INTO categoria (nome) VALUES ('Farmácia');
-INSERT INTO categoria (nome) VALUES ('Outros');
+CREATE TABLE categorias(
+  codigo SERIAL,
+  nome VARCHAR(20),
+  CONSTRAINT categoria PRIMARY KEY(codigo)
+);
+
+INSERT INTO categorias (nome) VALUES ('Lazer');
+INSERT INTO categorias (nome) VALUES ('Alimentação');
+INSERT INTO categorias (nome) VALUES ('Supermercado');
+INSERT INTO categorias (nome) VALUES ('Farmácia');
+INSERT INTO categorias (nome) VALUES ('Outros');
+
