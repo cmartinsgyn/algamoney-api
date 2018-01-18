4b36ab3a312e4fe7ebe642557deda72bf16e977d
Claudio <cmartinsgyn@gmail.com>
2018-01-18 18:47:20 -0200

    6.7 - accessToken  no cookie

---
+++ src/main/java/com/algaworks/algamoney/api/token/RefreshTokenPostProcessor.java
@@ -0,0 +1,54 @@
+package com.algaworks.algamoney.api.token;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> aClass) {
+        return returnType.getMethod().getName().equals("postAccessToken");
+    }
+
+    @Override
+    public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter returnType,
+                                             MediaType mediaType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                             ServerHttpRequest request, ServerHttpResponse response) {
+
+        HttpServletRequest req = ((ServletServerHttpRequest)request).getServletRequest();
+        HttpServletResponse resp = ((ServletServerHttpResponse)response).getServletResponse();
+
+        String refreshToken = body.getRefreshToken().getValue();
+        adicionarRefreshTokenNoCookie(refreshToken, req, resp);
+
+
+        //parei: 12 minutos
+        // implementar o postman. ver aula 6.6
+
+        return null;
+    }
+
+    private void adicionarRefreshTokenNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp) {
+
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true); // define se ele é acessível apenas em http
+        refreshTokenCookie.setSecure(false); // define se é ou não em https:
+        refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
+        refreshTokenCookie.setMaxAge(2592000); // cookie expira em 30 dias
+        resp.addCookie(refreshTokenCookie);
+
+
+    }
+}
