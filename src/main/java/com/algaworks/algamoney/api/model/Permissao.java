f67308f4b7a17318576ddc0e4400b2050dccfbb3
Claudio <cmartinsgyn@gmail.com>
2018-02-02 19:01:26 -0200

    6.11 - usuario em bd, begin...

---
+++ src/main/java/com/algaworks/algamoney/api/model/Permissao.java
@@ -0,0 +1,25 @@
+package com.algaworks.algamoney.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "permissao")
+public class Permissao {
+
+    @Id
+    private Long codigo;
+
+
+
+}
---
+++ src/main/java/com/algaworks/algamoney/api/model/Usuario.java
@@ -0,0 +1,35 @@
+package com.algaworks.algamoney.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    private Long codigo;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario-permissao", joinColumns = @JoinColumn(name="codigo_usuario"),
+            inverseJoinColumns = @JoinColumn(name = "codigo_permissao"))
+    List<Permissao> permissoes;
+
+}
