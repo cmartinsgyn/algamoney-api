3bb464014f6dd0c3e9ed3600af5ba6319f854fa6
claudio <cmartinsgyn@gmail.com>
2019-07-12 18:32:17 -0300

    acesso db heroku - 12/07/19 18:32

--- src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -5,13 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 
-    private String originPermitida = "https://algamoney-api-cms.herokuapp.com";
-
-    private final Seguranca seguranca = new Seguranca();
-
-    public Seguranca getSeguranca() {
-        return seguranca;
-    }
+    private String originPermitida = "http://localhost:4200";
 
     public String getOriginPermitida() {
         return originPermitida;
@@ -20,6 +14,14 @@ public class AlgamoneyApiProperty {
     public void setOriginPermitida(String originPermitida) {
         this.originPermitida = originPermitida;
     }
+
+
+    private final Seguranca seguranca = new Seguranca();
+
+    public Seguranca getSeguranca() {
+        return seguranca;
+    }
+
 
     public static class Seguranca {
 
